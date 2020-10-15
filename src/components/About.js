93d32bb9ef78618760f8bbec4788ca754dab3eaf
Samuel STPL <samuelstpierrelariviere@gmail.com>
2020-10-15 14:37:01 -0400

    React Ecosystem

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React from "react"
+import styled from "styled-components"
+
+
+const About = () => {
+    // console.log("About")
+    return (
+        <section>
+            <p>Fruit emporium is founder on a very simple principle: Fruit is goog</p>
+            <p>We carry the finest selection of produce from around the world, from tart citrus to sweet cherries. Our sellers are world-class, and your fruit is guaranteed to be worthy of auction in Asian markets.</p>
+        </section>
+    )
+}
+
+export {About}
--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React from "react";
+import { Header } from "./Header";
+import { About } from "./About";
+import { Home } from "./Home";
+import { ItemDetails } from "./ItemDetails";
+import { Switch, BrowserRouter as Router, Route, Link } from "react-router-dom";
+// import { createGlobalStyle } from "styled-components"
 
 const App = (props) => {
-  return 'Hello world!';
+  return (
+    <Router>
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route exact path="/items/:itemId">
+            <ItemDetails />
+          </Route>
+        </Switch>
+        {/* <Footer /> */}
+      </div>
+    </Router>
+  );
 };
 
 export default App;
